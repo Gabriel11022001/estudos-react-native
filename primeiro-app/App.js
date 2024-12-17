38ed5d516e9a871da4d035fb95b4ce7a7b3b2668
Gabriel Rodrigues dos Santos <gabriel996567521@gmail.com>
2024-12-17 20:07:34 -0300

    subindo o primeiro projetinho do curso para o github

---
+++ primeiro-app/App.js
@@ -0,0 +1,13 @@
+const { View, Text } = require('react-native')
+
+// todo componente no react native é uma função
+const App = () => {
+
+  return (
+    <View>
+      <Text>Primeiro projeto</Text>
+    </View>
+  )
+}
+
+export default App
